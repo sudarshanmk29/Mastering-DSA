6a97db3f06099676e434d168f32f461005ea6859
Sudarshan M K <54470983+sudarshanmk29@users.noreply.github.com>
2022-07-27 18:53:21 +0530

    Update Half_pyramid.cpp

--- Mastering Data Structures and Algorithms/Half_pyramid.cpp
+++ Mastering Data Structures and Algorithms/Half_pyramid.cpp
@@ -5,22 +5,22 @@
 * * * * 
 */
 
-// #include<iostream>
-// using namespace std;
-// int main()
-// {
-//     int n = 5;
-//     // cin>>n;
-//     for(int i=n;i>0;i--)
-//     {
-//         for(int j=0;j<=n;j++)
-//         {
-//             if(j>=i)
-//                 cout<<"*";
-//             else    
-//                 cout<<" ";
-//         }
-//         cout<<endl;
-//     }
-// }
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n = 5;
+    // cin>>n;
+    for(int i=n;i>0;i--)
+    {
+        for(int j=0;j<=n;j++)
+        {
+            if(j>=i)
+                cout<<"*";
+            else    
+                cout<<" ";
+        }
+        cout<<endl;
+    }
+}
 
